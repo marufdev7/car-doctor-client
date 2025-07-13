3a175d2a1402050c6db200273a26debcf47f2086
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-14 00:39:43 +0600

    login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import img from '../../assets/images/login/login.svg'
+
+const Login = () => {
+
+    const handleLogin = e => {
+        e.prevetDefault();
+    }
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="mr-12">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body">
+                        <h1 className="text-4xl font-semibold text-center">Login</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className='btn bg-[#FF3811] text-white hover:bg-red-600' type="submit" value="Login" />
+                            </div>
+                        </form>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
