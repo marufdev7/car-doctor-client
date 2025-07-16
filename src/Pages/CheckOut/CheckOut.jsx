573509d21c2d8ff712560fa2a3f93a32d19e6842
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-17 02:32:28 +0600

    get data from form

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,13 +1,107 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const CheckOut = () => {
     const service = useLoaderData();
-    const { title, service_id, price } = service;
+    const { title, price, _id} = service;
+    const { user } = useContext(AuthContext);
+
+    const handleOrderSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const message = form.message.value;
+        const orderData = {
+            customerName: form.name.value,
+            date: form.date.value,
+            email: form.email.value,
+            service: _id,
+            price: price,
+        };
+        console.log("Order submitted:",message, orderData);
+        form.reset();
+    };
     return (
-        <div>
-            <h2>Book Service:{title}</h2>
-        </div>
+        <>
+            <h2 className="text-3xl font-semibold text-center mb-6">Chosen Service: {title}</h2>
+            <div className="p-6 bg-base-200 rounded-lg shadow-md mx-auto my-10">
+                <form onSubmit={handleOrderSubmit} className="space-y-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                defaultValue={user?.displayName}
+                                placeholder="Name"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Date</span>
+                            </label>
+                            <input
+                                type="date"
+                                name="date"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                defaultValue={user?.email}
+                                placeholder="Your Email"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Due Amount</span>
+                            </label>
+                            <input
+                                type="text"
+                                // name="due"
+                                defaultValue={'$' + price}
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label className="label">
+                            <span className="label-text">Your Message</span>
+                        </label>
+                        <textarea
+                            name="message"
+                            className="textarea textarea-bordered w-full"
+                            placeholder="Your message here"
+                            rows="4"
+                        ></textarea>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn w-full text-white bg-[#FF3811] hover:bg-red-500"
+                    >
+                        Order Confirm
+                    </button>
+                </form>
+            </div>
+        </>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from './Routes/Router.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <div className='ml-24 mr-24 mx-auto'>
+    <div className='ml-4 lg:ml-24 mr-4 lg:mr-24 mx-auto'>
         <StrictMode>
             <AuthProvider>
                 <RouterProvider router={router} />
