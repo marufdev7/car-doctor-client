fb3c1df7e31d8d79444cc34ad3dc849364148b3f
mrfhmd7 <mrf.hmd7@gmail.com>
2025-07-20 20:49:56 +0600

    product card added

---
+++ src/Pages/Home/Products/ProductCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProductCard = ({ product }) => {
+    return (
+        <div className="border p-4 rounded-md shadow-sm hover:shadow-lg transition duration-300">
+            <img src={product.image} alt={product.name} className="w-full h-40 object-contain mb-4" />
+            <div className="text-center">
+                <div className="rating mb-2">
+                    {Array(product.rating)
+                        .fill()
+                        .map((_, idx) => (
+                            <span key={idx} className="text-orange-400 text-xl">★</span>
+                        ))}
+                </div>
+                <h3 className="font-semibold text-lg">{product.name}</h3>
+                <p className="text-[#FF3811] font-bold">${product.price.toFixed(2)}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
